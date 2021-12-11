927f3e1fc5a0b3f85541246189e258b146c42c2d
Bruno Lima Cardoso <blimacardoso@gmail.com>
2021-12-11 03:37:03 -0300

    feat(loggerdriver): console logger setup
    it creates and instanciates the console logger through the logger driver

--- tests/driver/test_logger_driver.py
+++ tests/driver/test_logger_driver.py
@@ -7,13 +7,13 @@ from tools.driver.logger_driver import LoggerDeriver
 
 
 class ConfigDriverTest(TestCase):
-    def setUp(self) -> None:
-        self.driver = LoggerDeriver()
-
+    @mock.patch.object(logging, "getLogger")
     @mock.patch.object(logging.config, "fileConfig")
-    def test_setup(self, mock_fileConfig):
+    def test_setup(self, mock_fileConfig, mock_getLogger):
         """
         it should setup the logger driver using the logging.ini file
+        and instanciate the logger
         """
         LoggerDeriver()
         mock_fileConfig.assert_called_once_with("logging.ini")
+        mock_getLogger.assert_called_once_with("console")
--- tools/driver/logger_driver.py
+++ tools/driver/logger_driver.py
@@ -5,7 +5,13 @@ import logging.config
 
 
 class LoggerDeriver:
-    """Class designed to access and handle logger operations"""
+    """Class designed to access and handle logger operations
+
+    Atributes:
+        logger_console (Logger): instance of a logger to write its outputs in
+        the console
+    """
 
     def __init__(self) -> None:
         logging.config.fileConfig("logging.ini")
+        self.logger_console = logging.getLogger("console")
