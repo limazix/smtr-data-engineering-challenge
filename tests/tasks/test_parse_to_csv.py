8efdef3bc70584e5ffa99724c2d46b2bea871cef
Bruno Lima Cardoso <blimacardoso@gmail.com>
2021-12-11 08:37:21 -0300

    feat(tasks): building header o parsing json to csv
    it uses a given bus status attributes to build the csv header when parsing the json to csv

--- tests/tasks/test_parse_to_csv.py
+++ tests/tasks/test_parse_to_csv.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+import json
+
 from tools.tasks.abs_task import ABSTask
 from tools.tasks.parse_to_csv_task import ParseToCSVTask
 
@@ -8,8 +10,23 @@ class ParseToCSVTaskTest(TestCase):
     def setUp(self) -> None:
         self.task = ParseToCSVTask()
 
+        with open("tests/tasks/mock_data.json") as d:
+            self.data = json.load(d)
+
     def test_setup(self):
         """
         it should be an ABSTask instance
         """
         self.assertIsInstance(self.task, ABSTask)
+
+    def test_build_header(self):
+        """
+        it should return all bus status attributes as
+        a string separated by comma
+        """
+        status = self.data[0]
+        keys = sorted(status.keys())
+        expected_header = ",".join(keys)
+
+        header = self.task.build_header(status)
+        self.assertEqual(expected_header, header)
--- tools/tasks/parse_to_csv_task.py
+++ tools/tasks/parse_to_csv_task.py
@@ -8,8 +8,24 @@ class ParseToCSVTask(ABSTask):
     Class designed to transform the JSON data into a CSV file
     """
 
-    def run(self):
+    def build_header(self, data: dict):
+        """
+        Method used to build the csv header
+
+        Attributes:
+            data (dict): dictionary with a single bus status
+
+        Returns:
+            str: a string with all column names separated by comma
+        """
+        headers = sorted(data.keys())
+        return ",".join(headers)
+
+    def run(self, data: list):
         """
         Method used to control the transformation process
+
+        Attributes:
+            data (list): list of all buses status
         """
         pass
