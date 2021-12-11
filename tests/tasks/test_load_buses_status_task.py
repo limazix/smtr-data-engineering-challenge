d76e125838ec28521e891da5d89cf8ffc189a9d5
Bruno Lima Cardoso <blimacardoso@gmail.com>
2021-12-11 07:30:02 -0300

    feat(tasks): load buses status
    it calls the api to load all buses latest status

--- tests/tasks/test_load_buses_status_task.py
+++ tests/tasks/test_load_buses_status_task.py
@@ -1,4 +1,6 @@
-from unittest import TestCase
+from unittest import TestCase, mock
+
+import requests
 
 from tools.tasks.abs_task import ABSTask
 from tools.tasks.load_buses_status_task import LoadBusesStatusTask
@@ -13,3 +15,12 @@ class LoadBusesStatusTaskTest(TestCase):
         it should be an ABSTask instance
         """
         self.assertIsInstance(self.task, ABSTask)
+
+    def test_run(self):
+        """
+        it should get all buses status from the datasource and return it
+        """
+        url = self.task.config.get_datasource_url()
+        with mock.patch.object(requests, "get") as mock_get:
+            self.task.run()
+            mock_get.assert_called_once_with(url)
--- tools/tasks/load_buses_status_task.py
+++ tools/tasks/load_buses_status_task.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import requests
+
 from .abs_task import ABSTask
 
 
@@ -9,4 +11,12 @@ class LoadBusesStatusTask(ABSTask):
 
     """
 
-    pass
+    def run(self):
+        """
+        Method used to load the latest buses status from the datasource
+
+        Returns:
+            list: All buses status
+        """
+        response = requests.get(self.config.get_datasource_url())
+        return response.json()
